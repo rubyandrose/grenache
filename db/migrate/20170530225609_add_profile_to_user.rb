fb49a5d3d79a64eb7419ab8136e8958aeb0a3ba8
Jon Mauney <jonathan@betterment.com>
2017-05-30 19:21:40 -0400

    Add user profile page

---
+++ db/migrate/20170530225609_add_profile_to_user.rb
@@ -0,0 +1,7 @@
+class AddProfileToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :birthday, :date
+    add_column :users, :location, :string
+    add_column :users, :hobbies, :text 
+  end
+end
