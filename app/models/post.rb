f6e1965e438a420f95854b0f2a7eb1a015faf2e7
Adrian Bautista <adrian.bautista@betterment.com>
2017-05-30 20:44:27 -0400

    Use allow_nil

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   EMOTIONS=['Happy', 'Sad', 'Intoxicated', 'Frump-tastic']
 
   belongs_to :user
-  validates :emotion, inclusion: { in: EMOTIONS, allows_blank: true }
+  validates :emotion, inclusion: { in: EMOTIONS }, allow_nil: true
 end
