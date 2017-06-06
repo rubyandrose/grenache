5ca9d76873e57999eed1b5c1584fa60f20bc015f
agirlnamedsophia <sophia.russell@betterment.com>
2017-06-06 19:06:18 -0400

    Creates Post Show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(user: current_user, content: params[:post][:content], emotion: params[:post][:emotion])
     @post.save
     redirect_to '/'
   end
-
 end
