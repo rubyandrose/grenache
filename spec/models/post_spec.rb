df981a55276c8df5812d8e423e0c046f6062318f
leahgoldberg <leah.goldberg@betterment.com>
2017-06-13 18:55:07 -0400

    Testing banned word is blocked out

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:new_post) {Post.create(content: 'Hey you jabroni')}
+  context 'when post contains banned words' do
+    it 'changes words to asterisks' do
+      expect(new_post.content).to eq 'Hey you *******'
+    end
+  end
 end
