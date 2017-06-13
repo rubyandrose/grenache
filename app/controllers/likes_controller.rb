e5667f8162e9ae3b3d81fae84a183ca001f1daa5
agirlnamedsophia <sophia.russell@betterment.com>
2017-06-13 19:05:08 -0400

    Remove js

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,17 +3,12 @@ class LikesController < ApplicationController
   def create
     @target_post = Post.find(params[:post_id])
     @target_post.likes.create!(user: current_user)
-
-    respond_to do |format|
-      format.js { render :re_render_likes }
-    end
+    redirect_to '/'
   end
 
   def destroy
     @target_post = Post.find(params[:post_id])
     @target_post.likes.find_by!(user: current_user).destroy
-    respond_to do |format|
-      format.js { render :re_render_likes }
-    end
+    redirect_to '/'
   end
 end
