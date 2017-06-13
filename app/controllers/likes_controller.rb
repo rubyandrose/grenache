0f67a8311deff1d92370cb612a5102cc36bfe8d9
agirlnamedsophia <sophia.russell@betterment.com>
2017-06-13 19:11:21 -0400

    Rails 5 snazziness

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
   def create
     @target_post = Post.find(params[:post_id])
     @target_post.likes.create!(user: current_user)
-    redirect_to '/'
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @target_post = Post.find(params[:post_id])
     @target_post.likes.find_by!(user: current_user).destroy
-    redirect_to '/'
+    redirect_back(fallback_location: root_path)
   end
 end
