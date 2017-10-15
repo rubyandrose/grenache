8073e7fb13698cc36b19c174790d7ce9d62be036
Adrian Bautista <adrianbautista@users.noreply.github.com>
2017-10-15 19:30:08 -0400

    Add basic post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    # same as Post.new(user: current_user, content: params[:content])
+    @post = current_user.posts.create(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
